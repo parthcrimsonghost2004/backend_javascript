a913b97e2585b6ab5b9674553fd778e89ee40e91
Parth <parthsanjay2004@gmail.com>
2026-08-05 01:51:35 +0530

    controller

--- src/app.js
+++ src/app.js
@@ -3,9 +3,14 @@ const app = express();
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 export {app}
+import userRouter from './routes/user.router.js'
 
 app.use(cors({origin: process.env.CORS_ORIGIN, credentials: true}))
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({extended:true, limit:"50mb"}))
 app.use(express.static('public'))
 app.use(cookieParser())
+
+app.use('/user',userRouter)
+
+// export default app
--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -7,15 +7,14 @@
 //     }
 // }
 
-const asynchandler = (fn) => { (req, res, next) => {
-        Promise.resolve(fn(req,res,next)).catch((error)=> {
+const asynchandler = (fn) => (req, res, next) => {
+    Promise.resolve(fn(req,res,next)).catch((error)=> {
         res.status(error.code||500).json({success:false, message:error.message})
     })
 }
-}
 
-
-// const asynchandler = (fn) => { (req, res, next) => {
+export default asynchandler
+// const asynchandler = (fn ) => { (req, res, next) => {
 //         Promise.resolve(fn(req,res,next)).catch((error)=> next(error))
 // }
 // }
